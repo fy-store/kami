bcedb3bf775756a871cd54e5a4660b433d98106b
feiYu <feiyucode@gmail.com>
2024-06-24 10:34:21 +0800

    [fix] 修复打包循环引用问题

--- src/db/connect/index.ts
+++ src/db/connect/index.ts
@@ -2,6 +2,7 @@ import { TInitTable, TQuery, TExecute } from '../types/index.js'
 import './init.js'
 import mysql2 from 'mysql2/promise'
 const { host, user, port, password, database } = config.mysql
+// import initTable from '../initTable/index.js'
 
 // 创建连接池，设置连接池的参数
 const pool = mysql2.createPool({
@@ -23,14 +24,11 @@ const pool = mysql2.createPool({
 	idleTimeout: 0
 })
 
-// 使用数据库
-await pool.query(`use ${database}`)
-
 export default pool
 export const execute: TExecute = pool.execute.bind(pool)
 export const query: TQuery = pool.query.bind(pool)
-
-const initTable: TInitTable = await import('../initTable/index.js')
-if (initTable.default) {
-	await initTable.default({ pool, execute, query })
-}
+import('../initTable/index.js').then(async (res) => {
+	if (res.default) {
+		await res.default({ pool, execute, query })
+	}
+})
