3918aacc7aea10147678d48f6d46b0ba975286db
feiYu <feiyucode@gmail.com>
2024-07-03 15:18:54 +0800

    [update] 优化

--- src/lib/defaultData/index.ts
+++ src/lib/defaultData/index.ts
@@ -1,6 +1,8 @@
 /**
+ * 设置对象默认值
  * @param target 目标对象
- * @param args 混入的对象数据
+ * @param args 默认值对象
+ * - 当 target 目标对象的数据为 undefind 或 不存在时, 将在默认值对象中挑选出对应的数据覆盖/写入 target 目标对象
  * @returns 目标对象
  */
 export default <T1 extends object, T2 extends object[]>(target: T1, ...args: T2): T1 & T2[number] => {
